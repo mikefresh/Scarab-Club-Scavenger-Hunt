a0b32ee3ea3433acf2e1bc2f0cf641e4d3d62cc6
Michael Evans <magic6435@gmail.com>
2021-04-16 09:18:07 -0400

    first pass

--- app.js
+++ app.js
@@ -1,16 +1,87 @@
 let db = firebase.firestore();
 
+let gameSession;
+
 function getPlaces() {
+  var places = [];
   db.collection("places")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((document) => {
-        console.log(document.data().name);
-        var marker = new mapboxgl.Marker()
-          .setLngLat([document.data().longitude, document.data().latitude])
-          .addTo(map);
+        places.push(document.data());
+        createPlaceMarker(document);
+        //console.log(document.data().name);
       });
+      getFivePlaces(places);
     });
 }
 
-getPlaces();
+function createPlaceMarker(document) {
+  var popup = new mapboxgl.Popup().setText(document.data().name).addTo(map);
+
+  var marker = new mapboxgl.Marker()
+    .setLngLat([document.data().longitude, document.data().latitude])
+    .addTo(map)
+    .setPopup(popup);
+}
+
+function getFivePlaces(places) {
+  //console.log(places[Math.floor(Math.random() * places.length)]);
+  var fivePlaces = [];
+  while (fivePlaces.length < 5) {
+    let place = places[Math.floor(Math.random() * places.length)];
+
+    if (fivePlaces.includes(place)) {
+      console.log("we found a duplicate");
+    } else {
+      fivePlaces.push(place);
+    }
+  }
+  console.log(fivePlaces);
+  createSession(fivePlaces);
+  getClue(fivePlaces[0]);
+}
+
+function getClue(place) {
+  console.log(place.clue);
+}
+
+function createSession(places) {
+  gameSession = {
+    places: [],
+    userId: 1234,
+    complete: false,
+    startTime: Date.now(),
+    endTime: null,
+    currentPlace: 0
+  };
+
+  gameSession.places = places;
+  console.log(gameSession);
+}
+
+function startGame() {
+  getPlaces();
+}
+
+function answerClue() {
+  gameSession.currentPlace++;
+  if (gameSession.currentPlace > 4) {
+    completeGame();
+  } else {
+    getClue(gameSession.places[gameSession.currentPlace]);
+  }
+}
+
+function completeGame() {
+  console.log("Game completed");
+  gameSession.endTime = Date.now();
+  gameSession.complete = true;
+  receiveBadge();
+}
+
+function receiveBadge() {
+  console.log("You got a badge!!!!");
+}
+
+startGame();
