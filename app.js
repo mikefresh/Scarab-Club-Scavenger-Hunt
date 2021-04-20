dbd4899ff341ff6ee4670647e83511779cdb2062
Michael Evans <magic6435@gmail.com>
2021-04-20 12:53:22 -0400

    auth

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+/// Auth stuff
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAjgkU7oRkq8ryphr66PKW5EziHOreT5X4",
+  authDomain: "scarab-club.firebaseapp.com",
+  projectId: "scarab-club",
+  storageBucket: "scarab-club.appspot.com",
+  messagingSenderId: "509020471771",
+  appId: "1:509020471771:web:5ae2b7c82516c5aaa167fe"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+/// Auth stuff
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+firebase.auth().onAuthStateChanged((firebaseuser) => {
+  if (firebaseuser) {
+    console.log(firebaseuser);
+    btnLogout.addEventListener("click", logout);
+    btnLogout.classList.remove("hide");
+  } else {
+    btnLogout.classList.add("hide");
+    ui.start("#firebaseui-auth-container", {
+      signInOptions: [
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: "https://fgb9m.csb.app/"
+    });
+  }
+});
+
+function logout() {
+  firebase.auth().signOut();
+}
+
+/// Database stuff
+
 let db = firebase.firestore();
 
 let gameSession;
